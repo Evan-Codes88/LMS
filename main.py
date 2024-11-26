a1883e06ab543d50501f60690adfb83d86811876
Evan-pixelperfect <evan2401tm@gmail.com>
2024-11-26 11:10:14 +1000

    Implemented read operation on student entity

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask
 from init import db, ma
 from controllers.cli_controller import db_commands
+from controllers.student_controller import students_bp
 
 def create_app():
     app = Flask(__name__)
@@ -13,6 +14,7 @@ def create_app():
     ma.init_app(app)
 
     app.register_blueprint(db_commands)
+    app.register_blueprint(students_bp)
 
     return app
 
