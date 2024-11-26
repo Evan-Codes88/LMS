fc7bb2926e1e598210c725f77e296c9ba6731321
Evan-pixelperfect <evan2401tm@gmail.com>
2024-11-26 14:26:51 +1000

    Implemented update on student entity

--- controllers/student_controller.py
+++ controllers/student_controller.py
@@ -6,12 +6,6 @@ from models.student import Student, students_schema, student_schema
 
 students_bp = Blueprint("students", __name__, url_prefix="/students")
 
-
-
-
-# Update - /students/id - PUT, PATCH
-
-
 # Read all - /students - GET
 @students_bp.route("/")
 def get_students():
@@ -33,28 +27,29 @@ def get_student(student_id):
     
 
 # Create - /students - POST
-@students_bp.route("/", methods = ["POST"])
+@students_bp.route("/", methods=["POST"])
 def create_student():
     try:
-        # Get information from body
+        # get information from the request body
         body_data = request.get_json()
-        # Create student instance
+        # create student instance
         new_student = Student(
             name = body_data.get("name"),
             email = body_data.get("email"),
             address = body_data.get("address")
         )
-        # Add to session
+        # add to the session
         db.session.add(new_student)
-        # Commit
+        # commit
         db.session.commit()
-        # Return a response
+        # return a response
         return student_schema.dump(new_student), 201
     except IntegrityError as err:
         if err.orig.pgcode == errorcodes.NOT_NULL_VIOLATION:
+            # not null violation
             return {"message": f"The field '{err.orig.diag.column_name}' is required"}, 409
-        
         if err.orig.pgcode == errorcodes.UNIQUE_VIOLATION:
+            # unique constraint violation
             return {"message": "Email address already in use"}, 409
         
 # Delete - /students/id - DELETE
@@ -74,3 +69,24 @@ def delete_student(student_id):
         # Return Error Response
     else:
         return {"message": f"Student with id {student_id} does not exist"}, 404
+    
+
+# Update - /students/id - PUT, PATCH
+@students_bp.route("/<int:student_id>", methods = ["PUT", "PATCH"])
+def update_student(student_id):
+    try:
+        stmt = db.select(Student).filter_by(id = student_id)
+        student = db.session.scalar(stmt)
+        body_data = request.get_json()
+
+        if student:
+            student.name = body_data.get("name") or student.name
+            student.email = body_data.get("email") or student.email
+            student.address = body_data.get("address") or student.address
+            db.session.commit()
+            return student_schema.dump(student)
+        else:
+            return {"message": f"Student with id {student_id} does not exist"}, 404
+
+    except IntegrityError as err:
+        return {"message": "Email address already in use"}, 409
